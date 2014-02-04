0d1b131416f3eafc2978cf7625066d19667ff05a
jakedsouza <jakedsouza88@gmail.com>
2014-02-04 00:48:30 -0800

    First simple video play from webcam

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,25 @@
 
 angular.module('webrtcApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+
+   navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+   var constraints = {audio: false, video: true};
+
+   var video;
+   function successCallback(stream) {
+      video = document.querySelector('video');
+      window.stream = stream; // stream available to console
+      if (window.URL) {
+         video.src = window.URL.createObjectURL(stream);
+      } else {
+         video.src = stream;
+     }
+   video.play();
+   }
+
+   function errorCallback(error){
+     console.log('navigator.getUserMedia error: ', error);
+   }
+
+   navigator.getUserMedia(constraints, successCallback, errorCallback);
+});
